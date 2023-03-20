84e9846a8baac4c152db0bec5f76fb4d8e9648f4
Kaylynn <mkaylynn7@gmail.com>
2023-03-20 11:57:16 +1100

    Improve handling of error reason

--- website/src/error.rs
+++ website/src/error.rs
@@ -7,6 +7,7 @@ use axum::{
     middleware::Next,
     response::{IntoResponse, Response},
 };
+use std::borrow::Cow;
 
 pub async fn to_error_page<B>(state: State, request: Request<B>, next: Next<B>) -> Response
 where
@@ -23,18 +24,46 @@ where
         return response;
     }
 
-    let message = match hyper::body::to_bytes(response.body_mut()).await {
+    let body_content = match hyper::body::to_bytes(response.body_mut()).await {
         Ok(bytes) => String::from_utf8_lossy(&bytes).into_owned(),
         Err(_) => return response, // How did this happen? Who knows.
     };
 
-    let error_page = Page::new(
-        "error",
-        context!("reason" => format!("Status code {}: {}", response.status().as_u16(), message)),
-    );
+    // If there was no explicit message, we should try and derive one from the status.
+    let message = match body_content.trim().is_empty() {
+        true => response
+            .status()
+            .canonical_reason()
+            .map(sentence_case)
+            .map(Cow::from),
+        false => Some(body_content.trim().into()),
+    };
 
-    error_page  
+    // However not all status codes have associated canonical messages. If so, we'll just give up. Maybe later we can do
+    // something funny, I dunno.
+    let code = response.status().as_u16();
+    let reason = match message {
+        Some(message) => format!("Status code {}: {}", code, message),
+        None => format!("Status code {} (no details provided...)", code),
+    };
+
+    Page::new("error", context!("reason" => reason))
         .render(state.engine())
         .map(|html| (response.status(), html))
         .into_response()
+}
+
+fn sentence_case(sentence: &str) -> String {
+    match sentence.chars().next() {
+        Some(first) => {
+            let first_size = first.len_utf8();
+
+            let mut result = sentence.to_owned();
+            result[..first_size].make_ascii_uppercase();
+            result[first_size..].make_ascii_lowercase();
+
+            result
+        }
+        _ => String::new(),
+    }
 }
